fdd7d8e4451bf8c081c80794e8ad0f7806d753ce
Andrey Dmitriev <admit8490@gmail.com>
2023-02-27 19:23:53 +0200

    testing events

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,23 @@
 function Meme() {
+  const handleClick = () => {
+    console.log('i was clicked');
+  }
   return (
     <form>
-      <input className="form--text-field top" type="text" placeholder="shut up" />
-      <input className="form--text-field bottom" type="text" placeholder="and take my money" />
-      <button className="form--newmeme">Get a new meme image</button>
+      <input
+        className="form--text-field top"
+        type="text"
+        placeholder="shut up" />
+      <input
+        className="form--text-field bottom"
+        type="text"
+        placeholder="and take my money" 
+      />
+      <button        
+        className="form--newmeme"
+        type="button"
+        onClick={handleClick}
+      >Get a new meme image</button>
     </form>
   );
 }
